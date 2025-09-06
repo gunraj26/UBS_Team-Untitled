1c80a3fb25295c6c699e1c4414b4055d2a0d3f7b
gunraj26 <gunraj262005@gmail.com>
2025-09-07 05:23:02 +0800

    .

--- routes/chasetheflag.py
+++ routes/chasetheflag.py
@@ -1,19 +1,19 @@
 from flask import Flask, jsonify, request
 
-app = Flask(__name__)
+from routes import app
 
 @app.route("/trivia", methods=["POST"])
 def chasetheflag():
     return jsonify({
        "answers": [
-    2,
-    1,
-    2,
-    2,
-    3,
-    4,
-    2,
-    5,
-    4, 
-  ]
+        2,
+        1,
+        2,
+        2,
+        3,
+        4,
+        2,
+        5,
+        4, 
+      ]
     })
